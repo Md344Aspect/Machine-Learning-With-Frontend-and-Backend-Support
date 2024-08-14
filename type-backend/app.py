bb2a5091baf30dd8b59a2d28e9f16951fc73b50d
Aspect <kajroprak@proton.me>
2024-08-14 15:33:31 -0700

    Update app.py

--- type-backend/app.py
+++ type-backend/app.py
@@ -1,1 +1,24 @@
+## init flask and server
+from flask import Flask, request, jsonify
+import joblib as jl
+import numpy as n
 
+app = Flask(__name__)
+
+# loading rfc model classification
+model = jl.load('model.pkl')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    ## feats 👇
+    data = request.json
+    ## get feats from req request and make a reshaped 1, -1 array using numpy(defined in imports) 👇 
+    features = n.array(data['features']).reshape(1, -1)
+    ## after the array is done its used in the rfc classification for the prediction 👇
+    prediction = model.predict(features)
+    ## getting first index of the prediction from the rfc classification 👇
+    return jsonify({'prediction': prediction[0]})
+
+if __name__ == '__main__':
+    ## active
+    app.run(debug=True)
